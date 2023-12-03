34d58f9b1f35b5888eb9bce45c8581437d332ffb
dommich95 <dominikmichel1995@gmail.com>
2023-12-03 17:21:06 +0100

    fix: remvoed unused variable

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { useTheme } from 'next-themes';
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import React from "react";
+import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 const ToggleTheme = () => {
-  const { systemTheme, theme, setTheme } = useTheme();
-  const currentTheme = theme === 'system' ? systemTheme : theme;
+  const { theme, setTheme } = useTheme();
 
   return (
     <button
-      onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
+      onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
       className="bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 p-2 rounded-full w-[40px] h-[40px] flex items-center justify-center mr-4"
     >
-      {theme === 'dark' ? (
+      {theme === "dark" ? (
         <SunIcon color="black" />
       ) : (
         <MoonIcon color="white" />
